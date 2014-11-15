613d65fe4d5fa7bf209f6ee05f616f46bbadfb28
thegrudge87 <dimitar411locals@gmail.com>
2014-11-15 20:33:18 +0200

    solvation problem 10

---
+++ Homework 03/10.InCircleOutRectangle/InCircleOutRectangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+    class InCircleOutRectangle
+    {
+        static void Main()
+        {
+            Console.Write("x = ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("y = ");
+            double y = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double r = 1.5;
+
+            bool isOutOfRectangle = (y>1)? true : false;
+            //(x - center_x)^2 + (y - center_y)^2 < radius^2
+            bool isInCircle = Math.Pow((x - 1), 2) + Math.Pow((y - 1),2) <= r * r;
+            Console.WriteLine( (isOutOfRectangle && isInCircle)? "yes" : "no");
+        }
+    }
---
+++ Homework 03/ConsoleApplication1/OddOrEvenIntegers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class OddOrEvenIntegers
+{
+    static void Main()
+    {
+        int n1 = 3;
+        int n2 = 2;
+        int n3 = -2;
+        int n4 = -1;
+        int n5 = 0;
+        Console.WriteLine((Math.Abs(n1) % 2 == 1) ? n1 + " is it Odd? - True" : n1 + " is it Odd - False");
+        Console.WriteLine((Math.Abs(n2) % 2 == 1) ? n2 + " is it Odd? - True" : n2 + " is it Odd - False");
+        Console.WriteLine((Math.Abs(n3) % 2 == 1) ? n3 + " is it Odd? - True" : n3 + " is it Odd - False");
+        Console.WriteLine((Math.Abs(n4) % 2 == 1) ? n4 + " is it Odd? - True" : n4 + " is it Odd - False");
+        Console.WriteLine((Math.Abs(n5) % 2 == 1) ? n5 + " is it Odd? - True" : n5 + " is it Odd - False");
+    }
+}
+
