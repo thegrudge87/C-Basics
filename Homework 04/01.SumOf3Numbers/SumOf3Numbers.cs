ecdbb9d0cebb6b63a4820ef567a6d430a117f051
thegrudge87 <dimitar411locals@gmail.com>
2014-11-17 15:56:31 +0200

    adding first files of Homeworks 04

---
+++ Homework 04/01.SumOf3Numbers/SumOf3Numbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+class SumOf3Numbers
+{
+    static void Main()
+    {
+        Console.Write("a = ");
+        double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("b = ");
+        double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("c = ");
+        double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("{0} + {1} + {2} = {3}", a, b, c, a+b+c);
+    }
+}
