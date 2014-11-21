f9b4cb63b779485741720a46a5a0941497ae306d
thegrudge87 <dimitar411locals@gmail.com>
2014-11-21 16:01:50 +0200

    solutions for problem #15, Homework #4

--- Homework 04/15.NewHouse/NewHouse.cs
+++ Homework 04/15.NewHouse/NewHouse.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _15.NewHouse
+class NewHouse
 {
-    class NewHouse
+    static void Main()
     {
-        static void Main(string[] args)
+        // Input data
+        byte n = byte.Parse(Console.ReadLine());
+
+        // The roof
+        for (int i = 0; i < n/2 + 1; i++)
         {
+            string roofOutLine = new string('-', Math.Abs(n / 2 - i));
+            string roof = new string('*', 2*i+1);
+            Console.WriteLine("{0}{1}{0}",roofOutLine, roof);
+        }
+
+        // The house
+        for (int i = 0; i < n; i++)
+        {
+            string corners = new string('|', 1);
+            string walls = new string('*',n-2);
+            Console.WriteLine("{0}{1}{0}", corners, walls);
         }
     }
 }
