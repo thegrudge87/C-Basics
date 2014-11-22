6ca875091bc71cbe99c51c0cb88a3ff954ef9c21
thegrudge87 <dimitar411locals@gmail.com>
2014-11-22 16:17:41 +0200

    improving code

--- Homework 03/19.TheExplorer/TheExplorer.cs
+++ Homework 03/19.TheExplorer/TheExplorer.cs
@@ -3,46 +3,37 @@ class TheExplorer
 {
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine()); // n = diamond width
+        // n = diamond width
+        int n = int.Parse(Console.ReadLine()); 
 
         for (int i = 0; i < n; i++)
         {
+            // first and last line
             if (i == 0 || i == n - 1)
             {
-                // first and last line
                 string outDiamond = new string('-', n / 2);
-                string diamondLine = new string('*', 1);
-                Console.WriteLine("{0}{1}{0}", outDiamond, diamondLine);
+                Console.WriteLine("{0}{1}{0}", outDiamond, "*");
             }
+            // the central line
+            else if (i == (n/2))
+            {
+                string innerDiamond = new string('-', n - 2);
+                Console.WriteLine("{0}{1}{0}", "*", innerDiamond);
+            }
+            // top of the diamond
+            else if (i < (n/2))
+            {
+                string outDiamond = new string('-', n / 2 - i);
+                string innerDiamond = new string('-', 2*(i-1)+1);
+                Console.WriteLine("{0}{1}{2}{1}{0}", outDiamond, "*", innerDiamond);
+            }
+            // bottom of the diamond
             else
             {
-                // the central line
-                if (i == (n/2))
-                {
-                    string diamondLine = new string('*', 1);
-                    string innerDiamond = new string('-', n - 2);
-                    Console.WriteLine("{0}{1}{0}", diamondLine, innerDiamond);
-                }
-                else
-                {
-                    // top of the diamond
-                    if (i < (n/2))
-                    {
-                        string outDiamond = new string('-', n / 2 - i);
-                        string diamondLine = new string('*', 1);
-                        string innerDiamond = new string('-', 2*(i-1)+1);
-                        Console.WriteLine("{0}{1}{2}{1}{0}", outDiamond, diamondLine, innerDiamond);
-                    }
-                    else
-                    {
-                        // bottom of the diamond
-                        string outDiamond = new string('-', Math.Abs(n/2-i)); 
-                        string diamondLine = new string('*', 1);
-                        // n-2 = count of the middle; 2*(i - n/2) - distance from the middle
-                        string innerDiamond = new string('-', Math.Abs(n-2 - 2*(i - n/2))); 
-                        Console.WriteLine("{0}{1}{2}{1}{0}", outDiamond, diamondLine, innerDiamond);
-                    }
-                }
+                string outDiamond = new string('-', Math.Abs(n/2-i)); 
+                // n-2 = count of the middle; 2*(i - n/2) - distance from the middle
+                string innerDiamond = new string('-', Math.Abs(n-2 - 2*(i - n/2)));
+                Console.WriteLine("{0}{1}{2}{1}{0}", outDiamond, "*", innerDiamond);
             }
         }
     }
