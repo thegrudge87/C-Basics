b6154d17db4468386ea7d200fa489f2c16d07a44
thegrudge87 <dimitar411locals@gmail.com>
2014-11-23 16:33:41 +0200

    adding solutions for problems #1, #17 from HW #4

---
+++ Homework 04/16.MagicStrings/MagicStrings.cs
@@ -0,0 +1,98 @@
+﻿using System;
+class MagicStrings
+{
+    static void Main()
+    {
+        // input data 
+        int diff = int.Parse(Console.ReadLine());
+        char[] letters = { 's', 'n', 'k', 'p' };
+        Array.Sort(letters);
+        bool isMagicStrExist = false;
+
+        // loop for the 1st letter of the sequence
+        for (int ch1 = 0; ch1 < letters.Length; ch1++)
+        {
+            // loop for 2nd letter of the sequence
+            for (int ch2 = 0; ch2 < letters.Length; ch2++)
+            {
+                // loop for 3th letter of the sequence
+                for (int ch3 = 0; ch3 < letters.Length; ch3++)
+                {
+                    // loop for 4th letter of the sequence
+                    for (int ch4 = 0; ch4 < letters.Length; ch4++)
+                    {
+                        // ::: IN THIS PART I'M GETTING THE VALUE OF LEFT 4 LETTERS :::
+                        // must use empty string to show the compilator that we want to use concatination.
+                        string firstFour = "" + letters[ch1] + letters[ch2] + letters[ch3] + letters[ch4];
+                        int leftStrValue = 0;
+                        foreach (var symbol in firstFour)
+                        {
+                            switch (symbol)
+                            {
+                                case 's':
+                                    leftStrValue += 3;
+                                    break;
+                                case 'n':
+                                    leftStrValue += 4;
+                                    break;
+                                case 'k':
+                                    leftStrValue += 1;
+                                    break;
+                                case 'p':
+                                    leftStrValue += 5;
+                                    break;
+                            }
+                        }
+                        // loop for 5th letter of the sequence
+                        for (int ch5 = 0; ch5 < letters.Length; ch5++)
+                        {
+                            // loop for 6th letter of the sequence
+                            for (int ch6 = 0; ch6 < letters.Length; ch6++)
+                            {
+                                // loop for 7th letter of the sequence
+                                for (int ch7 = 0; ch7 < letters.Length; ch7++)
+                                {
+                                    // loop for 8th letter of the sequence
+                                    for (int ch8 = 0; ch8 < letters.Length; ch8++)
+                                    {
+                                        string secondFour = "" + letters[ch5] + letters[ch6] + letters[ch7] + letters[ch8];
+                                        int rightStrValue = 0;
+                                        foreach (var symbol in secondFour)
+                                        {
+                                            switch (symbol)
+                                            {
+                                                case 's':
+                                                    rightStrValue += 3;
+                                                    break;
+                                                case 'n':
+                                                    rightStrValue += 4;
+                                                    break;
+                                                case 'k':
+                                                    rightStrValue += 1;
+                                                    break;
+                                                case 'p':
+                                                    rightStrValue += 5;
+                                                    break;
+                                            }
+                                        }
+
+                                        // ::: CHECK IS THE VALUES OF THE LEFT & RIGHT PARTS HAVE DIFFERENCE  = diff  :::
+                                        if (Math.Abs(leftStrValue-rightStrValue)==diff)
+                                        {
+                                            Console.WriteLine(firstFour+secondFour);
+                                            isMagicStrExist = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (isMagicStrExist == false)
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
---
+++ Homework 04/17.CatchTheBits/CatchTheBits.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class CatchTheBits
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int step = int.Parse(Console.ReadLine());
+
+        int currentElemement = 0;
+        int output = 0;
+        int count = 0;
+
+        // loop for the N elements that should be input;
+        for (int i = 0; i < n; i++)
+        {
+            // input the current N element
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            // loop goes through all 8 bits of the current element
+            for (int bitN = 7; bitN >= 0; bitN--)
+			{
+                // check that the current element:
+                // must be greater than equal element with 1. (1 + XX*step)
+                // OR 
+                // is the step = 1 AND in the same time the current element position is bigger than 0;
+			    if ((currentElemement % step == 1) || (step == 1 & currentElemement > 0))
+	            {
+                    int bitValue = (inputNumber >> bitN) & 1;
+                    output = (output << 1) | bitValue;
+                    count++;
+                    // check is there 8 bits in the output
+                    // if there is we write it on the console and reset output and the bit counter.
+                    if (count == 8)
+	                {
+                        Console.WriteLine(output);
+                        output = 0;
+                        count = 0;
+	                }
+	            }
+                currentElemement++;
+			}
+        }
+        // if there is a byte with less than 8 bits, we must add trailing zeroes from the right.
+        if (count > 0)
+	    {
+            Console.WriteLine("{0}",(output << (8-count)));
+	    }
+    }
+}
