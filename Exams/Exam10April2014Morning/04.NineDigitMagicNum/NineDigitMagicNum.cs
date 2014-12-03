abd8d3a59a698c754137502535dc9eace8118426
thegrudge87 <dimitar411locals@gmail.com>
2014-12-03 15:26:42 +0200

    adding solution for BitInventer

---
+++ Exams/Exam10April2014Morning/04.NineDigitMagicNum/NineDigitMagicNum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class NineDigitMagicNum
+{
+    static void Main()
+    {
+        // Input
+        int sum = int.Parse(Console.ReadLine());
+        int diff = int.Parse(Console.ReadLine());
+
+        // Bool if we have found a magic number it will become true.
+        bool isMagicNum = false;
+
+        for (int abc = 111; abc <= 777; abc++)
+        {
+            for (int def = 111; def <= 777; def++)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Exams/Exam10April2014Morning/05.BitsInverter/BitsInverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class BitsInverter
+{
+    static void Main()
+    {
+        // input
+        int n = int.Parse(Console.ReadLine());
+        int step = int.Parse(Console.ReadLine());
+        int counter = 0;
+        string output ="";
+
+        // Loop for n numbers that will be inputted.
+        for (int i = 0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());  // 00101101
+            // Loop that goes from all 8bits from left to right
+            for (int bit = 7; bit >= 0; bit--)
+            {
+                counter++;
+                if ((step==1) || (counter%step == 1))
+                {
+                    num = num ^ (1 << bit);
+                }
+            }
+            output = output + num + "\n";
+        }
+        if (counter == 0)
+        {
+            Console.WriteLine("No");
+        }
+        else
+        {
+            Console.WriteLine(output);
+        }
+    }
+}
