ce46687f9bbfae7bac52379495fcf776da69dfb0
thegrudge87 <dimitar411locals@gmail.com>
2014-12-04 16:27:08 +0200

    addin solution

---
+++ Exams/PrivateLabNovember/02.Num/Nums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Nums
+{
+    static void Main()
+    {
+        // Input data
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        for (int i = start; i <= end; i++)
+        {
+            if (i%2 == 0)
+            {
+                Console.WriteLine("{0:F3}", Math.Sqrt(i));
+                //Console.WriteLine("{0:F3}", Math.Pow(i, 2));
+            }
+            else
+            {
+                //Console.WriteLine("{0:F3}", Math.Sqrt(i));
+                Console.WriteLine("{0:F3}", Math.Pow(i, 2));
+            }
+        }
+    }
+}
